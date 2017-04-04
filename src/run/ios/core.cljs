9eba42efb8140bdb5ed0da35aa51edf597702129
dumeng <dumeng81@outlook.com>
2017-04-04 19:30:08 +0800

    add map support; add realm

--- src/run/ios/core.cljs
+++ src/run/ios/core.cljs
@@ -3,9 +3,14 @@
             [re-frame.core :refer [subscribe dispatch dispatch-sync]]
             [run.events]
             [run.subs]
-            [run.common.rn :refer [tabbar-ios
+            [run.common.rn :refer [app-registry
+                                   view
+                                   text
+                                   image
+                                   tabbar-ios
                                    tabbar-ios-item
-                                   switch]]
+                                   switch
+                                   touchable-highlight]]
             ))
 
 (def ReactNative (js/require "react-native"))
@@ -15,12 +20,6 @@
 (def Icon (js/require "react-native-vector-icons/FontAwesome"))
 (def tabbar-item-ios (r/adapt-react-class (.-TabBarItemIOS Icon)))
 
-
-(def app-registry (.-AppRegistry ReactNative))
-(def text (r/adapt-react-class (.-Text ReactNative)))
-(def view (r/adapt-react-class (.-View ReactNative)))
-(def image (r/adapt-react-class (.-Image ReactNative)))
-(def touchable-highlight (r/adapt-react-class (.-TouchableHighlight ReactNative)))
 
 (def logo-img (js/require "./images/cljs.png"))
 
@@ -39,20 +38,92 @@
                              :on-press #(alert "HELLO!")}
         [text {:style {:color "white" :text-align "center" :font-weight "bold"}} "press you"]]])))
 
+(defn zoom
+  [this factor]
+  (let [region (:region (r/state this))
+        lat-d (:latitude-delta region)
+        long-d (:longitude-delta region)]
+    (print "zoomed .." factor)
+    (r/set-state this (merge region  {:latitude-delta (* lat-d factor)
+                                      :longitude-delta (* long-d factor)}))))
+
 (defn view-one
   []
-  [map-view {:initial-region {:latitude 37.78825
-                              :longitude 122.4324
-                              :latitude-delta 0.0922
-                              :longitude-delta  0.0421
-                              }
-             :style {:width 100
-                     :background-color "yellow"
-                     :height 200}}])
+  (r/create-class {:get-initial-state (fn [this] {:location "fetching..."
+                                                 :region {:latitude 37.78825
+                                                          :longitude 122.4324
+                                                          :latitude-delta 0.222
+                                                          :longitude-delta  0.221
+                                                          }})
+                   :component-will-mount (fn
+                                           [this]
+                                           (.getCurrentPosition
+                                            (.-geolocation js/navigator )
+                                            (fn [pos]
+                                              (r/set-state this {:region (merge (:region (r/state this))
+                                                                                {:latitude (-> pos .-coords .-latitude)
+                                                                                 :longitude (-> pos .-coords .-longitude)
+                                                                                 })
+                                                                 :location  (-> pos .-coords .-latitude)}) )))
+                   :render (fn [this]
+                             [view
+                              [text {:style {:margin-top 50}} (:location (r/state this))]
+                              [map-view {:initial-region (:region (r/state this ))
+                                         :style {:flex-grow 1
+                                                 :background-color "yellow"
+                                                 :height 400}}]
+                              [touchable-highlight {:on-press (fn [] (zoom this 2.0))}
+                               [text "+"]]
+                              [touchable-highlight {:on-press (fn [] (zoom this 0.5))}
+                               [text "-"]]]
+                             )}))
+
+(defn view-1 []
+  (let [region (r/atom {:latitude 31.78825
+                        :longitude 121.4324
+                        :latitude-delta 0.222
+                        :longitude-delta  0.221
+                        })
+        location (r/atom "fetching...")
+        lat-d (r/cursor region [:latitude-delta])
+        long-d (r/cursor region [:longitude-delta])]
+    (fn []
+; (js/setTimeout (fn [] (reset! location "fetche ......") 1000))
+      (.getCurrentPosition (.-geolocation js/navigator) (fn [pos] (swap! region merge {:latitude (.-coords.latitude pos)
+                                                                                      :longitude (.-coords.longitude pos)})))
+      [view
+       [text {:style {:margin-top 50}} @location]
+       [map-view {:initial-region @region
+                  :region @region
+                  :style {:flex-grow 1
+                          :background-color "yellow"
+                          :height 400}}]
+       [touchable-highlight {:on-press (fn [] (reset! location "shrink") (swap! region merge {:latitude-delta (* @lat-d 2)
+                                                                                             :longitude-delta (* @long-d 2)}))}
+        [text "+"]]
+       [touchable-highlight {:on-press (fn [] (reset! location "increase") (swap! region merge {:latitude-delta (* @lat-d 0.5)
+                                                                                               :longitude-delta (* @long-d 0.5)}))}
+        [text "-"]]])))
+
+;; schema for realm
+(def Realm (js/require "realm"))
+(def dog-schema {:name "Dog"
+                 :properties {:owner "string"
+                              :age "float"}})
+
+(def realm (Realm. (clj->js {:schema [dog-schema]})))
+
+(defn view-two
+  []
+  (fn []
+    (.write realm #(.create realm "Dog" (clj->js {:owner "dumeng" :age 3.2})))
+    [view {:style {:margin-top 50}}
+     [text "view 3"]
+     [text (str "Dog now has " (.-length (.objects realm "Dog")))]]))
 
 (defn tabbar-view
   []
-  (let [selected (atom 0)]
+  (let [selected (atom 2)]
     (fn []
       [tabbar-ios
        [tabbar-item-ios {:title "one"
@@ -65,12 +136,12 @@
                          :selected (= @selected 1)
                          :on-press #(reset! selected 1)}
 
-        [view-one]]
+        [view-1]]
        [tabbar-item-ios {:title "three"
                          :icon-name "cog"
                          :selected (= @selected 2)
                          :on-press #(reset! selected 2)}
-        [text "view two"]]])))
+        [view-two]]])))
 
 
 (defn app-root []
